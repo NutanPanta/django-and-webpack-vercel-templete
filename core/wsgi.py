34e44812d529fe5bd0a27f70d636d3d2197b8b6d
wigglyfaun <nutanrajpanta@gmail.com>
2023-06-30 11:26:47 +0545

    changed public vairable name in wsgi from application to app

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,8 +1,8 @@
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-os.environ.setdefault('DJANGO_CONFIGURATION', 'Prod')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
 
 from configurations.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
+app = get_wsgi_application()

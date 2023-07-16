db41642e777ecb4dc3ea830e0c233ab18b959f55
wigglyfaun <nutanrajpanta@gmail.com>
2023-07-16 14:01:30 +0545

    configured webpack for dev and prod

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 python3 manage.py migrate
-python3 manage.py collectstatic
+# python3 manage.py collectstatic
 npm run build
 uwsgi --http :8000 --wsgi-file /opt/luxury_tour_in_nepal/core/wsgi.py
